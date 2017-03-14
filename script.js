4f48eec1a8c26a8b4679a5b1e5d897060b9cad26
tylerschroeder7 <schro266@msu.edu>
2017-03-14 15:27:06 -0500

    No console log and mouse enter added w comments

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-  var boomButton = document.getElementById('boomButton')
-  var boomSound = document.getElementById('boom')
-  boomButton.addEventListener('click', function () {
-    console.log('I was clicked!')
-    boomSound.play()
-
-  })
-
-  var openHatButton = document.getElementById('openHatButton')
-  var openHatSound = document.getElementById('openHat')
-
-  openHatButton.addEventListener('click', function () {
-    openHatSound.play()
-  })
-
-  var snareButton = document.getElementById('snareButton')
-  var snareSound = document.getElementById('snare')
-
-  snareButton.addEventListener('click', function () {
-    snareSound.play()
-  })
+// clicks and mouseover makes boom sound//
+var boomButton = document.getElementById('boomButton')
+var boomSound = document.getElementById('boom')
+boomButton.addEventListener('click', function () {
+  boomSound.play()
+})
+boomButton.addEventListener('mouseenter', function () {
+  boomSound.play()
+})
+// clicks and mouseover makes open hat sound//
+var openHatButton = document.getElementById('openHatButton')
+var openHatSound = document.getElementById('openHat')
+openHatButton.addEventListener('click', function () {
+  openHatSound.play()
+})
+openHatButton.addEventListener('mouseenter', function () {
+  openHatSound.play()
+})
+// clicks and mouseover makes snare sound//
+var snareButton = document.getElementById('snareButton')
+var snareSound = document.getElementById('snare')
+snareButton.addEventListener('click', function () {
+  snareSound.play()
+})
+snareButton.addEventListener('mouseenter', function () {
+  snareSound.play()
+})
